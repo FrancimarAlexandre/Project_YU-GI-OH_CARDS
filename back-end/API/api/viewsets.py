7eacfef647f6d37424d8d3b7d7fade28fe7a4ef6
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-03-04 12:39:28 -0300

    iniciando autenticação JWT

--- back-end/API/api/viewsets.py
+++ back-end/API/api/viewsets.py
@@ -1,6 +1,7 @@
 from rest_framework import viewsets
 from API.api import serializers
 from API import models
+
 class UsuarioViewset(viewsets.ModelViewSet):
     serializer_class = serializers.UsuarioSerializers
     queryset = models.Usuario.objects.all()
