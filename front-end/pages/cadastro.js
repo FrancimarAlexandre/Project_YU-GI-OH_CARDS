32644c1861da271ba8b7982877a6684cc4c28988
FrancimarAlexandre <francimaralexandredt@gmail.com>
2024-05-29 09:14:27 -0300

    iniciando lógica cadastro

--- front-end/pages/cadastro.js
+++ front-end/pages/cadastro.js
@@ -1,19 +1,65 @@
 // pages/login.js
-import React from 'react';
+import React, {useState} from 'react';
 import Link from 'next/link';
 import styles from '../styles/Login.module.css';
 
 export default function Cadastro() {
+  const [data, setData] = useState({
+    username :'',
+    email: '',
+    password:''
+  });
+
+  const handleChange = (e) =>{
+    const {name, value} = e.target;
+    setData(prevData => ({
+      ...prevData,
+      [name]: value
+    }));
+  };
+
+ const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://127.0.0.1:8000/usuario/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      });
+      const result = await response.json();
+      console.log('Success:', result);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
   return (
-    <div className={styles.loginContainer}>
-      <form className={styles.form}>
-        <h1>Login</h1>
-        <input type="email" placeholder="Email" className={styles.inputField} />
-        <input type="password" placeholder="Senha" className={styles.inputField} />
-        <button type="submit">Entrar</button>
-        <a href="/login">
-          <p>Já tem uma conta? Login</p>
-        </a>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="username"
+          value={data.username}
+          onChange={handleChange}
+          placeholder="Username"
+        />
+        <input
+          type="email"
+          name="email"
+          value={data.email}
+          onChange={handleChange}
+          placeholder="Email"
+        /> 
+         <input
+        type="password"
+        name="password"
+        value={data.password}
+        onChange={handleChange}
+        placeholder="Password"
+      />
+        <button type="submit">Enviar</button>
       </form>
     </div>
   );
